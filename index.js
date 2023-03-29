93f7afcab70d72eb5484d47d473acf0c43a361c9
loctvl842 <loclepnvx@gmail.com>
2023-03-29 16:57:41 +0700

    perf: improve `mqtt`
      - `mqtt` connect only once
      - only subcribe topic once for each device

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 8400;
+const axios = require("axios");
+require("dotenv").config();
 
-const ADAFRUIT_IO_USERNAME = "tamquattnb123";
-const ADAFRUIT_IO_KEY = "aio_SHSq53Ktw0oOxm77yXrUWZRm9Rt9";
+const PORT = 8400;
+const { ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY } = process.env;
+
+app.use(express.json());
 
 app.get("/", (_, res) => {
   res.send("Server is launched");
@@ -21,9 +24,7 @@ const mqtt = require("mqtt");
 const { Server } = require("socket.io");
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
+  cors: { origin: "http://localhost:3000" },
 });
 
 const mqttClient = mqtt.connect("wss://io.adafruit.com:443/mqtt/", {
@@ -31,17 +32,28 @@ const mqttClient = mqtt.connect("wss://io.adafruit.com:443/mqtt/", {
   password: ADAFRUIT_IO_KEY,
 });
 
-mqttClient.on("connect", () => console.log("mqtt: connected"));
+mqttClient.on("connect", async () => {
+  try {
+    const res = await axios.get(
+      `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds`
+    );
+    const feeds = res.data;
+    feeds.forEach((feed) => {
+      const topic = `${ADAFRUIT_IO_USERNAME}/feeds/${feed.key}`;
+      console.log(topic)
+      mqttClient.subscribe(topic);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+mqttClient.on("disconnect", () => console.log("mqtt: disconnected"));
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("subcribe", (feedKey) => {
-    const topic = `${ADAFRUIT_IO_USERNAME}/feeds/${feedKey}`;
-    mqttClient.subscribe(topic);
-  });
+  console.log("socket.id: ", socket.id);
+});
 
-  mqttClient.on("message", (topic, message) => {
-    const data = JSON.parse(message.toString());
-    socket.emit(topic, data);
-  });
+mqttClient.on("message", (topic, message) => {
+  const data = JSON.parse(message.toString());
+  io.emit(topic, data);
 });
